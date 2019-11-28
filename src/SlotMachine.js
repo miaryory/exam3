ddda1c03c747115f904faafa93eb5f604c86732b
ourangelofsorrows <angelezrameyer@gmail.com>
2019-11-28 22:52:48 +0100

    did some tests. I can now generate a random image

--- src/SlotMachine.js
+++ src/SlotMachine.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Game from "./Game";
+
 export default function SlotMachine() {
   return (
     <div>
       <h1>This is the SlotMachine area</h1>
+      <Game />
     </div>
   );
 }
