ff28ce8368dd4eae0aa2978e9cdd5c9190a6be59
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-12-01 17:15:31 +0100

    header added

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,28 @@
 import React from "react";
+import "./Header.css";
+import logo from "./temp_assets/logo.png";
+
 export default function Header() {
   return (
     <header>
-      <h1>This is the Header area</h1>
+      <img id="logoImg" src={logo}></img>
+
+      <nav id="desktop-nav">
+        <ul>
+          <li>
+            <a href="#home">Section1</a>
+          </li>
+          <li>
+            <a href="#news">Section2</a>
+          </li>
+          <li>
+            <a href="#contact">Section3</a>
+          </li>
+          <li>
+            <a href="#about">Log in</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
