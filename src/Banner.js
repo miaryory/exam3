5b4865c36fdafd96d09e87bcf3be40e4ced0d1ce
miaryory <miar0004@stud.kea.dk>
2019-12-05 20:20:24 +0100

    banner layout

--- src/Banner.js
+++ src/Banner.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./Banner.css";
+
 export default function Banner() {
   return (
-    <div>
-      <h1>This is the Banner area</h1>
+    <div className="banner">
+      <div className="banner-text">
+        <p className="countdown">Next draw: 2 days 05 h</p>
+        <h1>6 000 000 kr</h1>
+        <p className="slogan">Take your chance. Play Lucky 7.</p>
+      </div>
     </div>
   );
 }
