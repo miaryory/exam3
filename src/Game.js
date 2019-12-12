b0565400aceb1c11ffd43047206ea288243cd7a4
ourangelofsorrows <angelezrameyer@gmail.com>
2019-12-12 10:08:48 +0100

    animation work.

--- src/Game.js
+++ src/Game.js
@@ -7,15 +7,15 @@ const default_emoji2 = `./assets/dice/placeholder.svg`;
 const default_emoji3 = `./assets/dice/placeholder.svg`;
 const default_gamestatus = "";
 let counter = 0;
-let winstatus;
 
 
-
-function rungame() {
-  if (localStorage.getItem("winstatus") == "true") {
-    alert("you already won")
-  } else if (localStorage.getItem("tries") >= 3) {
-    alert("max 3 tries!")
+function rungame(current_gamestate) {
+  if (localStorage.getItem("winstatus") === "true") {
+    alert("you already won");
+    return current_gamestate;
+  } else if (localStorage.getItem("tries") >= 4) {
+    alert("max 3 tries!");
+    return current_gamestate;
   } else {
     counter++;
     let emoji1 = FindEmoji();
@@ -26,13 +26,13 @@ function rungame() {
     console.log(counter);
     localStorage.setItem("tries", counter);
     return {
+      ...current_gamestate,
       emoji1: emoji1,
       emoji2: emoji2,
       emoji3: emoji3,
       gamestatus: gamestatus
-    }
+    };
   }
-
 }
 
 function FindEmoji() {
@@ -45,7 +45,7 @@ function FindEmoji() {
 }
 
 function CheckForWin(emoji1, emoji2, emoji3) {
-  console.log(emoji1, emoji2, emoji3)
+  console.log(emoji1, emoji2, emoji3);
   let gamestatus = default_gamestatus;
 
   if (emoji1 === emoji2 && emoji2 === emoji3) {
@@ -55,64 +55,154 @@ function CheckForWin(emoji1, emoji2, emoji3) {
     gamestatus = "lose";
   }
   return gamestatus;
-
-
 }
 
 function alerting(gamestatus) {
-  if (gamestatus === "win") { alert("Congratulations! You just won 5 extra rows with your next Lucky7 purchase :)") }
-  if (gamestatus === "lose" && counter < 3) { alert("You lost, try again!") }
-  else if (gamestatus === "lose" && counter === 3) { alert("unfortunately you've lost and run out of tries.") }
+  if (gamestatus === "win") {
+    alert(
+      "Congratulations! You just won 5 extra rows with your next Lucky7 purchase :)"
+    );
+  }
+  else if (gamestatus === "lose" && counter < 4) {
+    alert("You lost, try again!");
+    console.log(gamestatus)
+
+  } else if (gamestatus === "lose" && counter === 4) {
+    alert("unfortunately you've lost and run out of tries.");
+  }
 }
-//reee this is not working yet aaaaaaaaaa
-/*function AlertFunk(gamestatus){
-  alert(gamestatus);
-}*/
 
 
 export default class Gameplace extends React.Component {
-  ///read react docs about this part - got that from there thanks to idw :)))
+  ///read react docs about this part
   constructor(props) {
     super(props);
     this.state = {
-      emoji1: default_emoji1,
-      emoji2: default_emoji2,
-      emoji3: default_emoji3,
-      gamestatus: default_gamestatus
-    };
-    this.spin = this.spin.bind(this); //this is a boilerplate for ES6 because of  a quirk thank u idw :)))
-
+      game_state: {
+        emoji1: default_emoji1,
+        emoji2: default_emoji2,
+        emoji3: default_emoji3,
+        gamestatus: default_gamestatus
+      },
+      animation_state: {
+        animation1: false,
+        animation2: false,
+        animation3: false,
+        waitForSpin: false
+      }
+    }
+    this.spin = this.spin.bind(this); //this is a boilerplate for ES6 because of  a quirk in JS: https://www.freecodecamp.org/news/this-is-why-we-need-to-bind-event-handlers-in-class-components-in-react-f7ea1a6f93eb/
+    this.animationEndAndUpdate = this.animationEndAndUpdate.bind(this);
   }
-
 
   //
   //spin! it spin!!!
-  spin() {
-    this.setState(rungame());
+  //spread syntax
+  spin(current_state) {
+    let new_state = {
+      ...current_state,
+      animation_state: { ...current_state.animation_state, waitForSpin: false },
+      game_state: rungame(current_state.game_state)
+    };
     //alerting();
+    return new_state;
+  }
+
+  //reset animations then spin
+
+  animationEndAndUpdate(new_state) {
+    if (new_state.animation_state.waitForSpin === true
+      && new_state.animation_state.animation1 === false
+      && new_state.animation_state.animation2 === false
+      && new_state.animation_state.animation3 === false) {
+      this.setState(this.spin(new_state));
+    } else {
+      this.setState(new_state);
+    }
   }
 
   /*note*/
   render() {
+    const spinClick = () => {
+      localStorage.setItem("subscribed", false);
+      if (localStorage.getItem("subscribed") === "false" && localStorage.getItem("tries") >= 1) {
+        alert("subscribe to our newsletter for 3 more tries");
+        console.log("subsribe form goes here")
+      } else {
+        this.setState({
+          ...this.state, animation_state: {
+            ...this.state.animation_state,
+            animation1: true,
+            animation2: true,
+            animation3: true,
+            waitForSpin: true
+          }
+        });
+      }
+    };
+    ///the animation code is heavily inspired by https://stackoverflow.com/questions/24111813/how-can-i-animate-a-react-js-component-onclick-and-detect-the-end-of-the-animati
+
+    const animation1end = () => {
+      this.animationEndAndUpdate({
+        ...this.state, animation_state: {
+          ...this.state.animation_state, animation1: false,
+        }
+      });
+    };
+
+    const animation2end = () => {
+      this.animationEndAndUpdate({
+        ...this.state, animation_state: {
+          ...this.state.animation_state, animation2: false,
+        }
+      });
+    };
+
+    const animation3end = () => {
+      this.animationEndAndUpdate({
+        ...this.state, animation_state: {
+          ...this.state.animation_state, animation3: false,
+        }
+
+      });
+    };
+
+    const defaultClassnames = "oneEmoji";
+    const animationClasses = "oneEmoji oneEmojiAnim";
+
     return (
-      <div id="fullGame" >
+      <div id="fullGame">
         <div id="mainRow">
           <div id="machineWrap">
-            <button onClick={this.spin} id="spinBTN">Spin</button>
-            <div id="whiteBox"></div>
+            <button onClick={() => spinClick()} id="spinBTN">
+              Spin
+            </button>
             <img src={machineImg} alt="slotmachine" id="machineImg" />
           </div>
           <div id="dice">
-            <img src={require(`${this.state.emoji1}`)} alt="random emoji" className="oneEmoji"></img>
-            <img src={require(`${this.state.emoji2}`)} alt="random emoji" className="oneEmoji"></img>
-            <img src={require(`${this.state.emoji3}`)} alt="random emoji" className="oneEmoji" id="lastemoji"></img>
+            <img
+              src={require(`${this.state.game_state.emoji1}`)}
+              alt="random emoji"
+              className={this.state.animation_state.animation1 ? animationClasses : defaultClassnames}
+              onAnimationEnd={() => animation1end()}
+            ></img>
+            <img
+              src={require(`${this.state.game_state.emoji2}`)}
+              alt="random emoji"
+              className={this.state.animation_state.animation1 ? animationClasses : defaultClassnames}
+              onAnimationEnd={() => animation2end()}
+            ></img>
+            <img
+              src={require(`${this.state.game_state.emoji3}`)}
+              alt="random emoji"
+              className={this.state.animation_state.animation1 ? animationClasses : defaultClassnames}
+              onAnimationEnd={() => animation3end()}
+              id="lastemoji"
+            ></img>
           </div>
-
-          <p>{this.state.gamestatus}</p>
+          <p> {this.state.gamestatus} </p>
         </div>
-
       </div>
-
     );
   }
 }
