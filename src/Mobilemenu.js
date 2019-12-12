cfb2a073fedad199e6bd09fa4b1cdd7ef8ac2b4b
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-12-12 02:48:50 +0100

    menu responsive

--- src/Mobilemenu.js
+++ src/Mobilemenu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import "./Header";
+import "./Header.css";
 import { slide as Menu } from "react-burger-menu";
 
 class Mobilemenu extends React.Component {
@@ -10,20 +10,29 @@ class Mobilemenu extends React.Component {
 
   render() {
     return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">
-          Home
-        </a>
-        <a id="about" className="menu-item" href="/about">
-          About
-        </a>
-        <a id="contact" className="menu-item" href="/contact">
-          Contact
-        </a>
-        <a onClick={this.showSettings} className="menu-item--small" href="">
-          Settings
-        </a>
-      </Menu>
+      <div className="menu-mobile">
+        <Menu>
+          <a className="menu-item" href="#home">
+            Section1
+          </a>
+
+          <a className="menu-item" href="#news">
+            Section2
+          </a>
+          <a onClick={this.showSettings} className="menu-item" href="#news">
+            Section3
+          </a>
+
+          {/* <a
+            onClick={this.showSettings}
+            className="menu-item"
+            onClick={openModal}
+            href="#about"
+          >
+            Log in
+          </a> */}
+        </Menu>
+      </div>
     );
   }
 }
