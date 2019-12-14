a8424acc5422c828e59261b2fd25614e8ad8c312
miaryory <miar0004@stud.kea.dk>
2019-12-14 15:24:55 +0100

    countdown added and banner responsive

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Banner.css";
+import Countdown from "./Countdown";
 
 export default function Banner() {
   return (
     <div className="banner">
       <div className="banner-text">
-        <p className="countdown">Next draw: 2 days 05 h</p>
-        <h1>6 000 000 kr</h1>
+        <div className="countdown">
+          <p>Next Draw: </p>
+          <Countdown />
+        </div>
+        <h1 className="price">6 000 000 kr</h1>
         <p className="slogan">Take your chance. Play Lucky 7.</p>
       </div>
     </div>
---
+++ src/Countdown.jsx
@@ -0,0 +1,83 @@
+//https://codepen.io/codecompio/pen/ybjqgv?editors=0010
+
+import React from "react";
+
+export default class CountDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timeInterval = null;
+    let nextTarget = this.figureOutNextTargetDate();
+    let targetDate = new Date(nextTarget);
+    this.targetDate = targetDate.getTime();
+    this.getTimeTill(this.targetDate, timeRemaining => {
+      this.state = {
+        timeRemaining
+      };
+    });
+  }
+
+  figureOutNextTargetDate() {
+    let current = new Date();
+    let nextdate = current.setDate(current.getDate() + 6);
+    return nextdate;
+  }
+
+  getTimeTill(targetTime, callback) {
+    let current = new Date();
+    let currentTime = current.getTime();
+    let millis = targetTime - currentTime;
+    let f = Math.floor;
+    let seconds = f(millis / 1000);
+    let minutes = f(seconds / 60);
+    let hours = f(minutes / 60);
+    let daysRemaining = f(hours / 24);
+    let hoursRemaning = hours % 24;
+    let minutesRemaning = minutes % 60;
+    let secondsRemaning = seconds % 60;
+
+    let timeRemaing = {
+      days: daysRemaining,
+      hours: hoursRemaning,
+      minutes: minutesRemaning,
+      seconds: secondsRemaning
+    };
+    callback(timeRemaing);
+  }
+  componentDidMount() {
+    this.timeInterval = setInterval(() => {
+      this.getTimeTill(this.targetDate, timeRemaining => {
+        this.setState({
+          timeRemaining
+        });
+      });
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timeInterval);
+  }
+
+  render() {
+    return (
+      <div id="countDown">
+        <section>
+          <div className="days-section">
+            <h1>{this.state.timeRemaining.days}</h1>
+            <p> days </p>
+          </div>
+          <div className="hours-section">
+            <h1>{this.state.timeRemaining.hours}</h1>
+            <p>hours</p>
+          </div>
+          <div className="minutes-section">
+            <h1>{this.state.timeRemaining.minutes}</h1>
+            <p>minutes</p>
+          </div>
+          <div className="seconds-section">
+            <h1>{this.state.timeRemaining.seconds}</h1>
+            <p>seconds</p>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
