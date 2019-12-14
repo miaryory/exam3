26d851db5a57596a03452bd2f7ceefabb4ed704f
miaryory <miar0004@stud.kea.dk>
2019-12-14 13:55:37 +0100

    winnig numbers preseneted in hexagon shape

--- src/WeekStatistics/WinningNumbers.jsx
+++ src/WeekStatistics/WinningNumbers.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Hexagon from "./hexagon.png";
 
 export default function WinningNumbers(props) {
   return (
     <div className="winning-numbers">
       {props.week.winningnumbers.map(number => {
-        return <p key={number}>{number}</p>;
+        return (
+          <div key={number} className="one-number">
+            <p>{number}</p>
+            <img className="hexagon" src={Hexagon} alt="Number" />
+          </div>
+        );
       })}
     </div>
   );
